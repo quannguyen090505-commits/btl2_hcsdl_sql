b2760ceef246b287e34b0746049181730fbad988
quannguyen090505 <quan.nguyen090505@hcmut.edu.vn>
2025-12-01 23:12:14 +0700

    Update btl2_sql.sql

--- btl2_sql.sql
+++ btl2_sql.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `btl2_hcsdl` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `btl2_hcsdl`;
 -- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: btl2_hcsdl
